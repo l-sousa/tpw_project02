51b4b50e49d83ce016517ac6b232529259166509
l-sousa <48656255+l-sousa@users.noreply.github.com>
2021-07-01 13:03:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/tpw-frontend'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/tpw-frontend/index.html'));});
-app.listen(process.env.PORT || 4200);
+
+app.use(requireHTTPS);
+app.use(express.static('./dist/tpw-frontend'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/tpw-frontend/'}),
+);
+
+app.listen(process.env.PORT || 8080);

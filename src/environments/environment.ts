f57fec07f824f8c94b58a85e910ab1a4debe8d21
l-sousa <48656255+l-sousa@users.noreply.github.com>
2021-07-01 18:14:39 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 import {HttpHeaders} from '@angular/common/http';
 
 export const environment = {
-  API_BASE_URL: "http://127.0.0.1:8000/ws/",
+  API_BASE_URL: "http://94.60.22.100/ws/",
   HTTP_OPTIONS: {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',

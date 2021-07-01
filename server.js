62bcb5ba264fb86a4bf0a063e7234b3dd42b422a
Lucas Sousa <joselmdbsousa@ua.pt>
2021-07-01 13:21:09 +0100

    update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/tpw-frontend'));
+app.use(express.static(__dirname + '/dist/teste'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/tpw-frontend/index.html'));});
+    '/dist/teste/index.html'));});
 app.listen(process.env.PORT || 4200);

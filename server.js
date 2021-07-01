92b4c1a0da5c18ed4cf4956dd097d235684295d0
Lucas Sousa <joselmdbsousa@ua.pt>
2021-07-01 12:41:12 +0100

    update

--- server.js
+++ server.js
@@ -13,5 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/tpw-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
+app.listen(process.env.PORT || 4200);

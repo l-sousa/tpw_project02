2f0ed594b4e78af43923af183948b20077593955
Lucas Sousa <joselmdbsousa@ua.pt>
2021-07-01 18:03:44 +0100

    udpates

--- src/app/category-products-list/category-products-list.component.ts
+++ src/app/category-products-list/category-products-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Location } from '@angular/common'; 
+import { Location } from '@angular/common';
 import { Emitters } from '../emitters/emitters';
 // MODEL
 import { Category } from '../models/Category';

4fe72ede50057e1dbc993e8b094523a4e22b69ba
Francisca Barros <kikas4323@gmail.com>
2021-07-01 11:17:58 +0100

    categories done

--- frontend/src/app/category-products-list/category-products-list.component.ts
+++ frontend/src/app/category-products-list/category-products-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common'; 
+import { Emitters } from '../emitters/emitters';
+// MODEL
 import { Category } from '../models/Category';
 import { Product } from '../models/Product';
-
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common'; 
+// SERVICES
 import { CategoryService } from '../services/category/category.service';
 
 @Component({
@@ -18,27 +20,34 @@ export class CategoryProductsListComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private location: Location,
     private categoryService: CategoryService
   ) { }
 
   ngOnInit(): void {
-    this.getBrand();
+    const id = Number(this.route.snapshot.paramMap['id']);
+    this.getCategory(id);
   }
 
-  getBrand(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+  addToShoppingCart(product: Product): void {
+    var key = "cart_item/" + product.id;
+    Emitters.newCartItemEmitter.emit(JSON.stringify(product));
+  }
+
+  getCategory(id: number): void {
     this.categoryService.getCategoryId(id).subscribe(category => this.category = category);
-    this.getCategoryProducts();
+    this.getCategoryProducts(id);
   }
 
-  getCategoryProducts(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+  getCategoryProducts(id: number): void {
     this.categoryService.getCategoryProducts(id).subscribe(products => this.products = products);
   }
 
   goBack(): void {
 		this.location.back();
+    this.router.navigate(['/']);
+    window.location.reload();
   }
 
 }
